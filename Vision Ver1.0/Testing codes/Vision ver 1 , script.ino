dd5fa2e83553552d12e56292e5c4cf85b9cbcaf7
Snehesh Dutta <53991169+Sneheshdutta@users.noreply.github.com>
2019-11-04 01:47:43 +0530

    Vision testing 1

---
+++ Vision Ver1.0/Testing codes/Vision ver 1 , script.ino
@@ -0,0 +1,60 @@
+//Digital torch for blind:
+const int ledPin = 13;
+const int speakerPin = 6;
+
+const int trigPin = 4;
+const int echoPin = 5;
+
+long duration;
+int distance, temp=-1;
+
+
+void setup() {
+ 
+    Serial.begin(9600);
+    pinMode(trigPin, OUTPUT);
+    pinMode(echoPin, INPUT);
+
+    pinMode(speakerPin,OUTPUT);
+    pinMode( ledPin , OUTPUT);
+
+    digitalWrite(ledPin, LOW);   
+}
+
+void loop() {
+  // Clears the trigPin
+  digitalWrite( trigPin , LOW);
+  delayMicroseconds(2);
+
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite( trigPin , LOW);
+  // Reads the echoPin, returns the sound wave
+  // travel time in microseconds
+  duration = pulseIn( echoPin , HIGH);
+
+  distance = duration * 0.034 / 2;
+
+
+
+  // Prints the distance on the Serial Monitor
+  Serial.print("Distance: ");
+  Serial.println(distance);
+
+   if( distance < 15 && distance > 0 ){
+
+       // Whoop up
+  for(int hz = 800; hz < 1000; hz++){
+    digitalWrite(ledPin,HIGH);
+    tone(speakerPin, hz, 50);
+    delay(distance);
+    digitalWrite(ledPin,LOW);
+  }
+  noTone(speakerPin);
+
+ 
+     
+   }
+
+  
+}
